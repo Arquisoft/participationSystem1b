0b292df3361763468e0ed569a94954ab2624e651
sergupe <sergupe6@gmail.com>
2017-03-29 14:23:13 +0200

    Solving build errors

--- src/test/java/hello/MainControllerTest.java
+++ src/test/java/hello/MainControllerTest.java
@@ -37,14 +37,14 @@ public class MainControllerTest {
 
 	@Test
 	public void getLanding() throws Exception {
-		String userURI = base.toString() + "/user";
+		String userURI = base.toString() + "/login";
 		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
 		assertThat(response.getBody(), containsString("Please"));
 	}
 	
 	@Test
 	public void getUser() throws Exception {
-		String userURI = base.toString() + "/user";
+		String userURI = base.toString() + "/login";
 		ResponseEntity<String> response = template.getForEntity(userURI, String.class);
 		UserInfo expected = new UserInfo("pepe",0);
 	}
