f3dad083a117a7ad73bc2b256d9eddf7d5b02ee1
sergupe <sergupe6@gmail.com>
2017-03-30 12:19:35 +0200

    Minor changes

--- src/main/java/hello/controllers/MainController.java
+++ src/main/java/hello/controllers/MainController.java
@@ -1,16 +1,29 @@
 package hello.controllers;
 
 
+import hello.Message;
+import hello.producers.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
     @RequestMapping("/")
     public String landing(Model model) {
-        return "index";
+        return "login";
+    }
+
+    @RequestMapping("/send")
+    public String send(Model model, @ModelAttribute Message message) {
+        kafkaProducer.send("exampleTopic", message.getMessage());
+        return "redirect:/";
     }
 
 }
--- src/test/java/hello/MainControllerTest.java
+++ src/test/java/hello/MainControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
+@SuppressWarnings("deprecation")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
