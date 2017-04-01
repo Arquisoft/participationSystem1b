89eefe304277fb98eb7a235576ab3984c41ce5c8
DavidGG1983 <uo173944@uniovi.es>
2017-04-01 18:17:37 +0200

    corregido error html

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,12 +1,14 @@
 package controllers;
 
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import model.CitizenDB;
+import services.impl.CitizenDBServiceImpl;
 
 @Controller
 @RequestMapping("*")
@@ -14,6 +16,23 @@ public class MainController {
 
     @RequestMapping(value="/")
     public String landing(Model model) {
+        return "login";
+    }
+    
+    
+    @RequestMapping(value="/user/home")
+    public String logHtml(Model model){
+    	return "user/home";
+    }
+    
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public String getLogin(@RequestParam String login, @RequestParam String password, Model mod){
+    CitizenDB user =  new CitizenDBServiceImpl().getCitizenDB(login);
+
+    if(user != null){
+         if(DigestUtils.sha512Hex(password).equals(user.getPassword()))
+               return "user/home";
+       }
         return "login";
     }
 }
