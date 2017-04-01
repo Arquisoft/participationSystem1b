bbb5d3c39137ea4394a6a6193f216d77fb352032
DavidGG1983 <uo173944@uniovi.es>
2017-04-01 17:56:54 +0200

    funcionando por fin....

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -3,19 +3,17 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import model.CitizenDB;
 
 @Controller
 @RequestMapping("*")
 public class MainController {
 
-    @RequestMapping(value="*")
-    public String landing(Model model) {
-        return "login";
-    }
-    
     @RequestMapping(value="/")
-    public String landing2(Model model) {
+    public String landing(Model model) {
         return "login";
     }
 }
