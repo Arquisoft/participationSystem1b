146b8e800ab4a19c2f882ce76d3ec2201520c934
DavidGG1983 <uo173944@uniovi.es>
2017-04-01 15:37:42 +0200

    error

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,14 +1,12 @@
 package controllers;
 
 
-import hello.Message;
 import producers.KafkaProducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
