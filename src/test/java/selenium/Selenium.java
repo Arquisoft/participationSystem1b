615b9f0549de3ad4521276f55fab792d807f0507
alexmatia <afmatia10@gmail.com>
2017-04-03 18:37:58 +0200

    Arreglos

--- src/test/java/selenium/Selenium.java
+++ src/test/java/selenium/Selenium.java
@@ -328,47 +328,5 @@ public class Selenium {
 		SeleniumUtils.textoNoPresentePagina(driver, "Sugerencia2");
 
 	}
-	
-	@After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
-
-	  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
-
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
-
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
 
 }
