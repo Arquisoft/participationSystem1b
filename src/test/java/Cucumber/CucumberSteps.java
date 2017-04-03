8a7b3146daf2a18bed03bcced5c05c86589401a7
Sergio Gutierrez Perez <sergupe6@gmail.com>
2017-04-03 12:57:50 +0200

    Cucumber Steps

--- src/test/java/Cucumber/CucumberSteps.java
+++ src/test/java/Cucumber/CucumberSteps.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.apache.commons.lang.SystemUtils;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -12,6 +13,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import controllers.Application;
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = Application.class)
@@ -34,6 +43,40 @@ public class CucumberSteps {
 
 	public static void tearDown() {
 		driver.quit();
+	}
+	
+	@After
+    public void cleanCookies() {
+        driver.manage().deleteAllCookies();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	@Given("^the user navigates to \"([^\"]*)\"$")
+	public void theUserNavigatesTo(String url) throws Throwable {
+		driver.get(url);
+	}
+
+	@When("^the user introduces username \"([^\"]*)\" and password \"([^\"]*)\"$")
+	public void theUserIntroducesUsernameAndPassword(String username, String password) throws Throwable {
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys(username);
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("login")).click();
+	}
+
+	@Then("^the login fails$")
+	public void theLoginFails() throws Throwable {
+		assertTrue(driver.getCurrentUrl().contains("/error"));
+	}
+
+	@Then("^the user successfully logs in$")
+	public void theUserSuccessfullyLogsIn() throws Throwable {
+		assertTrue(driver.getCurrentUrl().contains("/User/home"));
 	}
 
 }
